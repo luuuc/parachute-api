ab97e2926b4194bcdeca7413fd102bae6eb9af86
luc <luc@codedynamic.us>
2014-09-14 19:26:09 +0200

    Invitation model test

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Invitation, :type => :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a factory" do
+    expect(FactoryGirl.build(:invitation)).to be_valid
+  end
+
+  context "validations" do
+    subject { FactoryGirl.create(:invitation) }
+
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_inclusion_of(:language).in_array(I18n.available_locales.map(&:to_s)) }
+    it { should validate_inclusion_of(:time_zone).in_array(ActiveSupport::TimeZone.all.map(&:name)) }
+  end
+
+  context "private methods" do
+    it "should generate a 32 characters long token after creation" do
+      invitation = FactoryGirl.create(:invitation)
+      expect(invitation.token.length).to eq(32)
+    end
+  end
 end
